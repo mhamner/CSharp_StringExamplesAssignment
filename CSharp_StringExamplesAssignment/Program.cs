f18da46022593d9ae8af5a604170b7e7484dc753
mhamner <37221769+mhamner@users.noreply.github.com>
2021-06-07 17:13:38 -0400

    Add files via upload

---
+++ CSharp_StringExamplesAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_StringExamplesAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Allows the user to enter a string, then outputs the string back to them
+            Console.WriteLine("Please enter a string.");
+            string s = Console.ReadLine();
+            Console.WriteLine($"You entered: {s}");
+
+            Console.ReadKey();
+        }
+    }
+}
